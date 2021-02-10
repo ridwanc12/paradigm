fe5f5ef1dc8c43332398d0a384928a188b6ab44f
Isha Mahadalkar <ishamahadalkar03@gmail.com>
2021-02-10 15:11:36 -0500

    Connected UI elements to ViewControllers

--- Paradigm/LoginViewController.swift
+++ Paradigm/LoginViewController.swift
@@ -6,4 +6,46 @@
 //  Copyright © 2021 team5. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var restoreButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    
+    @IBAction func restoreButtonTapped (_ sender: UIButton) {
+        // When the restore password button is tapped
+        
+    }
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        // When the restore password button is tapped
+        let username: String = emailTextField.text ?? ""
+        let password: String = passwordTextField.text ?? ""
+        
+        print(username)
+        print(password)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view
+        
+        // Handling the text fields user input through delegate callbacks
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+//        emailTextField.layer.cornerRadius = 18
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+}
