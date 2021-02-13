0ba594b8a02796854f2cbfa7080d2a4d438aca31
Ridwan Chowdhury <rchowdhury1212@gmail.com>
2021-02-13 16:44:44 -0500

    Utilizing Latent Dirichlet Allocation with NLTK for topic modelling

---
+++ python_module/ind_scripts/LDA.py
@@ -0,0 +1,42 @@
+# Necessary modules from NLTK
+from nltk.stem import PorterStemmer 
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer 
+
+# Import Gensim
+import gensim
+  
+lemmatizer = WordNetLemmatizer() 
+stemmer = PorterStemmer() 
+
+journal_directory = 'journal_examples/'
+journal_name = journal_directory + 'positive1.txt'
+# journal_name = journal_directory + 'negative1.txt'
+
+with open(journal_name, 'r') as f:
+    content = f.readline()
+
+print(content)
+
+def lemmatize_stemming(text):
+    return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
+
+# Tokenize and lemmatize
+def preprocess(text):
+    result=[]
+    for token in gensim.utils.simple_preprocess(text) :
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
+            result.append(lemmatize_stemming(token))
+            
+    return result
+
+processed = preprocess(content)
+print(processed)
+
+dictionary = gensim.corpora.Dictionary([processed])
+print(dictionary)
+
+bow_corpus = [dictionary.doc2bow(processed)]
+print(bow_corpus)
+
+lda_model =  gensim.models.LdaMulticore(bow_corpus, num_topics = 8, id2word = dictionary, passes = 10, workers = 2)
