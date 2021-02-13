f9ee3114c3d9e9c5b3a86de2da76b831bd2f4aa5
Ridwan Chowdhury <rchowdhury1212@gmail.com>
2021-02-13 18:00:34 -0500

    Updated lambda with entity processing

--- python_module/lambda/lambda_function.py
+++ python_module/lambda/lambda_function.py
@@ -6,8 +6,9 @@ def sentiment_analysis(journal):
     comprehend = boto3.client(
         service_name='comprehend', region_name='us-east-2')
     sentiment = comprehend.detect_sentiment(Text=journal, LanguageCode='en')
-    topics = comprehend.detect_key_phrases(Text=journal, LanguageCode='en')
-    return [sentiment, topics]
+    phrases = comprehend.detect_key_phrases(Text=journal, LanguageCode='en')
+    entities = comprehend.detect_entities(Text=journal, LanguageCode='en')
+    return [sentiment, phrases, entities]
 
 
 def lambda_handler(event, context):
@@ -31,5 +32,5 @@ def lambda_handler(event, context):
         print(results)
         return {
             'statusCode': 200,
-            'body': json.dumps({'sentiment': results[0], 'topics': results[1]})
+            'body': json.dumps({'sentiment': results[0], 'phrases': results[1], 'entities': results[2]})
         }
