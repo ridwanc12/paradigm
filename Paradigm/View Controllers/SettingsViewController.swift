329a0fdf61b06a8ad002f0c2cd61983946420a99
Isha Mahadalkar <ishamahadalkar03@gmail.com>
2021-02-16 13:43:45 -0500

    Initial Home Screen Setup

---
+++ Paradigm/View Controllers/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  Paradigm
+//
+//  Created by Isha Mahadalkar on 2/16/21.
+//  Copyright © 2021 team5. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
