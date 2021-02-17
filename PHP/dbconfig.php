37d7b00354fd480148fc72fce5a46e3f2ec5ab72
Twnyang <yang1556@purdue.edu>
2021-02-17 00:56:20 -0500

    Remove unit test from config file

--- PHP/dbconfig.php
+++ PHP/dbconfig.php
@@ -1,4 +1,5 @@
 <?php
+
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
@@ -6,7 +7,6 @@ define('DB_USERNAME', 'id12866202_pete');
 define('DB_PASSWORD', 'Paradigm123!');
 define('DB_NAME', 'id12866202_paradigm');
 
- 
 /* Attempt to connect to MySQL database */
 try{
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
@@ -14,10 +14,6 @@ try{
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
     die("ERROR: Could not connect. " . $e->getMessage());
-}
-
-if ($pdo != null) {
-    echo "connected";
 }
 
 ?>
