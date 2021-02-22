5037391fc504c42551b15ae2acc5d2cb73fd84ac
Twnyang <yang1556@purdue.edu>
2021-02-21 21:00:07 -0500

    Modified getEntry to return all entries of the user

--- PHP/getEntry.php
+++ PHP/getEntry.php
@@ -4,34 +4,47 @@ require("./keys/keys.php");
 $param_email = $param_password = "";
 
 // Write SQL query to retrieve hashPass from table
-$sql = "SELECT entry FROM journals WHERE jourID = :jID";
+$sql = "SELECT * FROM journals WHERE userID = :userID";
 if ($stmt = $pdo->prepare($sql)) {
     // Bind variables to the prepared statement as parameters
-    $id = trim($_POST["jID"]);
-    $stmt->bindParam(":jID", $id, PDO::PARAM_INT);
+    $userID = trim($_POST["userID"]);
+    $stmt->bindParam(":userID", $userID, PDO::PARAM_INT);
 
     // Attempt to execute the prepared statement
     if ($stmt->execute()) {
-        // If rowcount == 1, email is already registered
-        if ($stmt->rowCount() == 1) {
-            // Set result to be associated with column name
-            $stmt->setFetchMode(PDO::FETCH_ASSOC);
-            // Fetch encrypted entry from result
-            $row = $stmt->fetch();
+        // Set result to be associated with column name
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        // Fetch encrypted entry from result
+        $all_entries = array();
+        while ($row = $stmt->fetch()) {
             $encrypted_entry = $row['entry'];
             // Open mcrypt module and buffer for decryption
             $mcrypt = mcrypt_module_open('rijndael-256', '', 'cbc', '');
             mcrypt_generic_init($mcrypt, $key, $iv);
             // Trim invisible padding characters from the decrypted entry
             $decrypted_entry = trim(mdecrypt_generic($mcrypt, base64_decode($encrypted_entry)), "\0\4");
-            echo $decrypted_entry;
+            // Close mcrypt module and buffer
             mcrypt_generic_deinit($mcrypt);
             mcrypt_module_close($mcrypt);
-            
-        } else {
-            // Email not registered
-            echo "Email not registered.";
+            $jourID = $row['jourID'];
+            $userID = $row['userID'];
+            $created = $row['created'];
+            $hidden = $row['hidden'];
+            $sentScore = $row['sentScore'];
+            $rating = $row['rating'];
+            $lastEdited = $row['lastEdited'];
+            $topics = $row['topics'];
+            $all_entries[$created] = array("jourID" => $jourID,
+                                           "userID" => $userID,
+                                           "created" => $created,
+                                           "hidden" => $hidden,
+                                           "sentScore" => $sentScore,
+                                           "rating" => $hidden,
+                                           "lastEdited" => $lastEdited,
+                                           "topics" => $topics); 
         }
+        $json = json_encode($all_entries);
+        echo $json;
     } else {
         echo "Oops! Something went wrong. Please try again later.";
     }
@@ -42,4 +55,3 @@ if ($stmt = $pdo->prepare($sql)) {
 
 // Close connection with database
 unset($pdo);
-?>
